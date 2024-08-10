a3e9fe6c2ee076d4c821fd15d53fd8dd18a19193
somu126 <132725166+somu126@users.noreply.github.com>
2024-08-10 10:55:28 +0530

    Update index.js

--- Frontend/src/services/index.js
+++ Frontend/src/services/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://backend-final1-cwl3.onrender.com"
 
 export const getHeaders = () => {
     let token = ""
